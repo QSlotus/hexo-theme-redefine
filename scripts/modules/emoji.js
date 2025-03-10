f886802fd8ccd7c060fcc22a271ca8ff9ab94522
QiuSYan <halo-402@outlook.com>
2025-03-10 19:54:07 +0800

    update: 同步博客主题文件

--- scripts/modules/emoji.js
+++ scripts/modules/emoji.js
@@ -11,8 +11,23 @@ function postEmoji(args) {
   }, {});
 
   const scale = options.scale || 1;
-  const style = `transform: scale(${scale}); display: inline-block; vertical-align: middle; cursor: default;`
-  return `<img src="https://s4.zstatic.net/npm/qiusyan-emojis@latest/${category}/${name}.png" data-no-preview="true" data-no-lazyload="true" style="${style}">`;
+  const width = options.width || 'auto'; // 支持自定义宽度
+  const height = options.height || 'auto'; // 支持自定义高度
+
+  const style = `
+    transform: scale(${scale});
+    display: inline-block;
+    vertical-align: baseline; 
+    cursor: default;
+    width: ${width};
+    height: ${height};
+    object-fit: contain;
+`;
+
+// 添加一个包裹元素，并设置首个子元素的样式
+return `<span style="display: inline-block; vertical-align: top;">
+  <img src="https://s4.zstatic.net/npm/qiusyan-emojis@latest/${category}/${name}.png" data-no-preview="true" data-no-lazyload="true" style="${style}">
+</span>`;
 }
 
 hexo.extend.tag.register('emoji', postEmoji);
